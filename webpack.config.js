86edc4549160d66fd5555ff2310f833ed3a0982f
Song Xie <me@xiesong.me>
2022-05-19 22:25:56 -0700

    Setup Webpack Dev Server

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,16 @@
 const path = require('path');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   context: path.resolve(__dirname, './src'),
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
+  },
   entry: {
     gameboy: './Gameboy',
   },
@@ -22,4 +30,9 @@ module.exports = {
     path: path.resolve(__dirname, './dist'),
     filename: '[name].bundle.js',
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+  ],
 };
